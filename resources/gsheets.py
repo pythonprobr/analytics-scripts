ad60a62ed40f734366e2b64e4cdb8ed29d8d2008
Moacir Moda <moa.moda@gmail.com>
2020-03-01 15:41:31 -0300

    removing unused var

--- resources/gsheets.py
+++ resources/gsheets.py
@@ -5,4 +5,3 @@ from settings import GOOGLE_ADS_SPREADSHEET_KEY
 gc = pygsheets.authorize(outh_file="client_secret.json")
 
 spreadsheet = gc.open_by_key(GOOGLE_ADS_SPREADSHEET_KEY)
-worksheet = spreadsheet.worksheet_by_title("Leads")
