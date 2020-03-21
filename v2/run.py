ae5b648e465a9a03635da018449b9a53a0d00c10
Moacir Moda <moa.moda@gmail.com>
2020-03-21 16:21:19 -0300

    adding parameter to handle with dates

--- v2/run.py
+++ v2/run.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 
 sys.path.append("./")
 
@@ -7,13 +8,20 @@ from v2.etl.user import ETLUser
 from v2.etl.session import ETLSession
 from v2.etl.lead import ETLLead
 from v2.etl.progress import ETLProgress
+from v2.etl.pageview import ETLPageView
 
-user = ETLUser()
-session = ETLSession()
-lead = ETLLead()
-progress = ETLProgress()
+date_until = datetime(2020, 3, 21)
+if "--full" in sys.argv:
+    date_until = datetime(2019, 12, 1)
+
+user = ETLUser(date_until)
+session = ETLSession(date_until)
+lead = ETLLead(date_until)
+progress = ETLProgress(date_until)
+pageview = ETLPageView(date_until)
 
 user.run()
-lead.run()
 session.run()
-progress.run()
+pageview.run()
+# lead.run()
+# progress.run()
