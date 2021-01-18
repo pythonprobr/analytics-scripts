923dab4377884528f7abe24dde6a047fb24d0549
Moacir Moda <moa.moda@gmail.com>
2021-01-18 16:25:37 -0300

    extracting joined and left information

--- extract-data-from-groups.py
+++ extract-data-from-groups.py
@@ -1,3 +1,4 @@
+import re
 import os
 import sys
 import zipfile
@@ -15,11 +16,13 @@ from settings import TIME_ZONE
 GLOB_STRING = "*#*.zip"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # log.info("Buscando numbers...")
 
     ppl_joined = {}
     ppl_left = {}
+    numbers_joined = []
+    numbers_left = []
 
     directory = sys.argv[1]
     count = 0
@@ -30,33 +33,59 @@ if __name__ == '__main__':
             with archive.open("_chat.txt") as handle:
                 for line in handle.read().decode().split("\n"):
                     line = line.strip()
-                    
-                    if line.endswith('left'):
+                    number = line.split("+")[-1]
+
+                    if "+" not in line:
+                        continue
+
+                    if line.endswith("left"):
                         date = line[1:9]
                         if date not in ppl_left:
                             ppl_left[date] = 0
                         ppl_left[date] += 1
-                    
-                    if 'joined using this group' in line:
+
+                        number = "+{}".format(number.split("left")[0].strip())
+                        number = number.strip()
+                        if number not in numbers_left:
+                            numbers_left.append(number)
+
+                    if "joined using this group" in line:
                         date = line[1:9]
                         if date not in ppl_joined:
                             ppl_joined[date] = 0
                         ppl_joined[date] += 1
+
+                        number = "+{}".format(number.split("joined")[0].strip())
+                        number = number.strip()
+                        if number not in numbers_joined:
+                            numbers_joined.append(number)
 
 total_ppl_joined = 0
 total_ppl_left = 0
 
 print()
 for date in ppl_joined:
-    print(f'-->> Dia {date}:')
-    print(f'Pessoas que entraram: {ppl_joined[date]}')
-    print(f'Pessoas que sairam: {ppl_left[date]}')
+    print(f"-->> Dia {date}:")
+    print(f"Pessoas que entraram: {ppl_joined[date]}")
+    print(f"Pessoas que sairam: {ppl_left[date]}")
     print()
 
     total_ppl_joined += ppl_joined[date]
     total_ppl_left += ppl_left[date]
 
 print()
-print(f'Total de pessoas que entraram: {total_ppl_joined}')
-print(f'Total de pessoas que sairam: {total_ppl_left}')
-print(f'Total de pessoas remanescentes: {total_ppl_joined - total_ppl_left}')
+print(f"Total de pessoas que entraram: {total_ppl_joined}")
+print(f"Total de pessoas que sairam: {total_ppl_left}")
+print(f"Total de pessoas remanescentes: {total_ppl_joined - total_ppl_left}")
+
+print()
+print("-->> Números:")
+for number in numbers_joined:
+    output = [re.sub("[^0-9]", "", number)]
+    if number not in numbers_left:
+        output.append("saiu")
+    else:
+        output.append("")
+
+    output.append(datetime.now().strftime("%d/%m/%Y %H:%M"))
+    print("\t".join(output))
